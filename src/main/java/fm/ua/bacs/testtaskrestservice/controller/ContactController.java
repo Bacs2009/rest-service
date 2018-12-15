5f5aaceaad3cbd89fd7fa4adbd0aa02d80cce055
Bacs <bacs@ua.fm>
2018-12-15 13:07:25 +0200

    Code has been cleared and corrected

--- src/main/java/fm/ua/bacs/testtaskrestservice/controller/ContactController.java
+++ src/main/java/fm/ua/bacs/testtaskrestservice/controller/ContactController.java
@@ -1,6 +1,5 @@
 package fm.ua.bacs.testtaskrestservice.controller;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import fm.ua.bacs.testtaskrestservice.model.Contact;
 import fm.ua.bacs.testtaskrestservice.repository.ContactRepository;
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,42 +16,23 @@ public class ContactController {
     @Autowired
     private ContactRepository contactRepository;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @GetMapping("/contacts")
-    public List<Contact> findByRegex(@RequestParam(name = "nameFilter") String nameFilter) throws UnsupportedEncodingException {
+    public List<Contact> findByRegex(@RequestParam(name = "nameFilter") String nameFilter) throws IOException {
 
         String filter = java.net.URLDecoder.decode(nameFilter, "UTF-8");
-        System.out.println("filter = " + filter);
 
-        ObjectMapper mapper = new ObjectMapper();
-        try {
-            String jsonInString = mapper.writeValueAsString(contactRepository.findAll());
-            System.out.println("contactRepository.findAll().stream() = " + jsonInString);
-        } catch (JsonProcessingException e) {
-            e.printStackTrace();
-        }
-
-        List<String> strings = new ArrayList<>(contactRepository.findAll().size());
-        for (Object object : contactRepository.findAll()) {
-            try {
-                String s = mapper.writeValueAsString(object);
-                if (!s.split(",")[1].split(":")[1].replace("\"", "").matches(filter)) {
-                    strings.add(mapper.writeValueAsString(object));
-                }
-            } catch (JsonProcessingException e) {
-                e.printStackTrace();
-            }
-        }
-
+        List<Contact> fromDb = contactRepository.findAll();
         List<Contact> result = new ArrayList<>();
-        try {
-            Contact[] contact = mapper.readValue(strings.toString(), Contact[].class);
 
-            for (Contact c :contact ) {
+        for (long i = 1; i < fromDb.size(); i++) {
+            Contact c = contactRepository.getOne(i);
+            String s = String.valueOf(c);
+            if (!s.split(",")[1].split("=")[1].replace("\"", "").matches(filter)) {
                 result.add(c);
             }
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
 
         return result;
